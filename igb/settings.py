7783cc9619507f186c6a1c4ad3e92e508c7375e2
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2019-05-21 16:36:31 +0100

    add django-environ and make custom settings loadable from environment

--- igb/settings.py
+++ igb/settings.py
@@ -18,44 +18,48 @@
 #
 #########################################################################
 
-# Django settings for the GeoNode project.
-import ast
 import os
-from urlparse import urlparse, urlunparse
 
-# Load more settings from a file called local_settings.py if it exists
-try:
-    from igb.local_settings import *
-#    from geonode.local_settings import *
-except ImportError:
-    from geonode.settings import *
+import environ
+from pathlib2 import Path
 
-#
-# General Django development settings
-#
-PROJECT_NAME = 'igb'
+from geonode.settings import *
 
-# add trailing slash to site url. geoserver url will be relative to this
-if not SITEURL.endswith('/'):
-    SITEURL = '{}/'.format(SITEURL)
-
-SITENAME = os.getenv("SITENAME", 'igb')
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Defines the directory that contains the settings file as the LOCAL_ROOT
 # It is used for relative settings elsewhere.
-LOCAL_ROOT = os.path.abspath(os.path.dirname(__file__))
+LOCAL_ROOT = str(Path(__file__).resolve().parent)
+
+PROJECT_NAME = 'igb'
+
+DEBUG = env("DEBUG")
+
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    "default": env.db_url("DJANGO_DATABASE_URL"),
+    "datastore": env.db_url("GEONODE_DB_URL"),
+}
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+
+SITEURL = env("DJANGO_SITEURL")
+
+SITENAME = env("DJANGO_SITENAME", default='igb')
 
 WSGI_APPLICATION = "{}.wsgi.application".format(PROJECT_NAME)
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = os.getenv('LANGUAGE_CODE', "en")
+LANGUAGE_CODE = env("LANGUAGE_CODE", default="en")
 
 if PROJECT_NAME not in INSTALLED_APPS:
     INSTALLED_APPS += (PROJECT_NAME,)
 
 # Location of url mappings
-ROOT_URLCONF = os.getenv('ROOT_URLCONF', '{}.urls'.format(PROJECT_NAME))
+# ROOT_URLCONF = os.getenv('ROOT_URLCONF', '{}.urls'.format(PROJECT_NAME))
+ROOT_URLCONF = '{}.urls'.format(PROJECT_NAME)
 
 # Additional directories which hold static files
 STATICFILES_DIRS.append(
@@ -68,13 +72,19 @@ LOCALE_PATHS = (
     ) + LOCALE_PATHS
 
 TEMPLATES[0]['DIRS'].insert(0, os.path.join(LOCAL_ROOT, "templates"))
-loaders = TEMPLATES[0]['OPTIONS'].get('loaders') or ['django.template.loaders.filesystem.Loader','django.template.loaders.app_directories.Loader']
+loaders = (
+    TEMPLATES[0]['OPTIONS'].get('loaders') or
+    [
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader'
+    ]
+)
 # loaders.insert(0, 'apptemplates.Loader')
 TEMPLATES[0]['OPTIONS']['loaders'] = loaders
 TEMPLATES[0].pop('APP_DIRS', None)
 
-UNOCONV_ENABLE = strtobool(os.getenv('UNOCONV_ENABLE', 'True'))
+UNOCONV_ENABLE = env("UNOCONV_ENABLE", cast=bool, default=True)
 
 if UNOCONV_ENABLE:
-    UNOCONV_EXECUTABLE = os.getenv('UNOCONV_EXECUTABLE', '/usr/bin/unoconv')
-    UNOCONV_TIMEOUT = os.getenv('UNOCONV_TIMEOUT', 30)  # seconds
+    UNOCONV_EXECUTABLE = env("UNOCONV_EXECUTABLE", default="/usr/bin/unoconv")
+    UNOCONV_TIMEOUT = env("UNOCONV_TIMEOUT", cast=int, default=30)  # seconds
